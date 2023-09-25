99e785ea4cde773e001099464e3baac1627dd925
savmg <skbmasale941@gmail.com>
2023-09-25 14:03:15 +0530

    Fn added for last5Months

---
+++ src/helpers/helper.js
@@ -0,0 +1,20 @@
+import { monthNames } from "../config/app-data";
+
+/**
+ * Calculates the name of the last 5 months
+ * @returns Array containing names of the last 5 months inclusively
+ */
+export function getLast5Month() {
+  let today = new Date();
+  const last5Months = [];
+
+  for (let i = 0; i < 5; i++) {
+    // Pushing monthname to an array
+    last5Months.push(monthNames[today.getMonth()]);
+
+    // Subtract 1 month from current month
+    today.setMonth(today.getMonth() - 1);
+  }
+
+  return last5Months;
+}
