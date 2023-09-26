fa8e72caafc9550f5461cf9938f02c591737250b
savmg <skbmasale941@gmail.com>
2023-09-26 20:06:05 +0530

    Navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
 import { useEffect } from "react";
-import HorizontalBarGraph from "./components/HorizontalBarGraph";
-import LineGraph from "./components/LineGraph";
-import PieGraph from "./components/PieGraph";
 import { starGrowthBarLast5Months } from "./services/filterDataForBarGraph";
+import AppShell from "./pages/AppShell";
 
 function App() {
   useEffect(function () {
     starGrowthBarLast5Months();
   }, []);
   return (
-    <div className="m-4">
-      <LineGraph />
-      <PieGraph />
-      <HorizontalBarGraph />
-    </div>
+    // <div className="m-4">
+    //   <LineGraph />
+    //   <PieGraph />
+    //   <HorizontalBarGraph />
+    // </div>
+    <AppShell/>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import 'bootstrap/dist/css/bootstrap.css'
-import { Provider } from 'react-redux'
-import store from './redux/store.js'
-ReactDOM.createRoot(document.getElementById('root')).render(
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import "@mantine/core/styles.css";
+import { Provider } from "react-redux";
+import store from "./redux/store.js";
+import { MantineProvider } from "@mantine/core";
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Provider store={store}> <App /></Provider>
-   
-  </React.StrictMode>,
-)
+    {" "}
+    <MantineProvider>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </MantineProvider>
+  </React.StrictMode>
+);
