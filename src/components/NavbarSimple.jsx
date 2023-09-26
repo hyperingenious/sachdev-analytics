2467f0387e48d6b31da05f4d6993f775ce3faf21
savmg <skbmasale941@gmail.com>
2023-09-26 21:59:43 +0530

    css moved to the assets

--- src/components/NavbarSimple.jsx
+++ src/components/NavbarSimple.jsx
@@ -1,44 +1,28 @@
 import { useState } from "react";
 import {
-  IconBellRinging,
-  IconFingerprint,
-  IconKey,
-  IconSettings,
-  Icon2fa,
-  IconDatabaseImport,
-  IconReceipt2,
   IconSwitchHorizontal,
   IconLogout,
+  IconDashboard,
 } from "@tabler/icons-react";
-import classes from "./NavbarSimple.module.css";
+import classes from "../assets/styles/Navbar.module.css";
+import { Link } from "react-router-dom";
 
-const data = [
-  { link: "", label: "Notifications", icon: IconBellRinging },
-  { link: "", label: "Billing", icon: IconReceipt2 },
-  { link: "", label: "Security", icon: IconFingerprint },
-  { link: "", label: "SSH Keys", icon: IconKey },
-  { link: "", label: "Databases", icon: IconDatabaseImport },
-  { link: "", label: "Authentication", icon: Icon2fa },
-  { link: "", label: "Other Settings", icon: IconSettings },
-];
+const data = [{ link: "/dashboard", label: "Dashboard", icon: IconDashboard }];
 
 export default function NavbarSimple() {
   const [active, setActive] = useState("Billing");
 
   const links = data.map((item) => (
-    <a
+    <Link
+      to={item.link}
       className={classes.link}
       data-active={item.label === active || undefined}
-      href={item.link}
-      key={item.label}
-      onClick={(event) => {
-        event.preventDefault();
-        setActive(item.label);
-      }}
+      key={item.link}
+      onClick={() => setActive(item.label)}
     >
       <item.icon className={classes.linkIcon} stroke={1.5} />
       <span>{item.label}</span>
-    </a>
+    </Link>
   ));
 
   return (
