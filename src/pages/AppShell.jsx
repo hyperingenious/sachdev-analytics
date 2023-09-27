ea7fadd6a3942fa1d36b3ffb71a7262a6cd8d2c3
savmg <skbmasale941@gmail.com>
2023-09-27 13:03:48 +0530

    dark theme added

--- src/pages/AppShell.jsx
+++ src/pages/AppShell.jsx
@@ -3,14 +3,18 @@ import { AppShell as MantineAppShell, Burger } from "@mantine/core";
 import NavbarSimple from "../components/NavbarSimple";
 import { Outlet, useNavigate } from "react-router-dom";
 import { useEffect } from "react";
+import ActionToggle from "../components/ActionToggle";
 
 export default function AppShell() {
   const [opened, { toggle }] = useDisclosure();
   const navigate = useNavigate();
 
-useEffect(function(){
-  navigate('/dashboard')
-},[navigate])
+  useEffect(
+    function () {
+      navigate("/dashboard");
+    },
+    [navigate]
+  );
 
   return (
     <MantineAppShell
@@ -18,9 +22,17 @@ useEffect(function(){
       navbar={{ width: 230, breakpoint: "sm", collapsed: { mobile: !opened } }}
       padding="md"
     >
-      <MantineAppShell.Header>
+      <MantineAppShell.Header
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          margin: "0 10px",
+        }}
+      >
         <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
-        <div>Sachdev Brothers</div>
+        <h6>Sachdev Brothers</h6>
+        <ActionToggle />
       </MantineAppShell.Header>
 
       <MantineAppShell.Navbar p="md">
