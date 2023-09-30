68367eef9d328046daadd78222d3121f46c104e1
savmg <skbmasale941@gmail.com>
2023-09-30 14:23:38 +0530

    Timer filter added in Reviews section

--- src/pages/Reviews.jsx
+++ src/pages/Reviews.jsx
@@ -2,7 +2,10 @@ import { Dropdown } from "../components/Dropdown";
 import SegmentedButton from "../components/SegmentedButton";
 import ReviewCard from "../components/reviews/ReviewCard";
 import { useDispatch, useSelector } from "react-redux";
-import { changeReviewDataWithRating } from "../redux/reviews/filterReviewsSlice";
+import {
+  changeReviewDataWithRating,
+  changeReviewDataWithTime,
+} from "../redux/reviews/filterReviewsSlice";
 
 function Reviews() {
   const { reviewData } = useSelector((state) => state.reviewsFilter);
@@ -23,6 +26,7 @@ function Reviews() {
           onClick={(arg) => dispatch(changeReviewDataWithRating(arg))}
         />
         <SegmentedButton
+          onChange={(arg) => dispatch(changeReviewDataWithTime(arg))}
           data={[
             { label: "7D", value: "7-day-time" },
             { label: "1M", value: "30-day-time" },
