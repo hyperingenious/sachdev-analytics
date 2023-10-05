c5601deb74cbb1e2607b45328bf93364d57a29ce
savmg <skbmasale941@gmail.com>
2023-10-06 02:07:12 +0530

    Modifined line graph

--- src/components/dashboard/LineGraph.jsx
+++ src/components/dashboard/LineGraph.jsx
@@ -17,6 +17,7 @@ import {
 } from "../../redux/dashboard/filterLineGraphSlice";
 import { Dropdown } from "../Dropdown";
 import SegmentedButton from "../SegmentedButton";
+import { Box, Card, Text } from "@mantine/core";
 
 function LineGraph() {
   const dispatch = useDispatch();
@@ -26,36 +27,67 @@ function LineGraph() {
   return (
     <div style={{ marginTop: "var(--mantine-spacing-xl)" }}>
       <LineGraphFilterBar dispatch={dispatch} />
-      <ResponsiveContainer height={200} width={"100%"}>
+      <ResponsiveContainer height={220} width={"100%"}>
         <AreaChart data={dataAsPerTime}>
-          <XAxis dataKey={"label"} />
-          <YAxis unit={"%"} />
-          <CartesianGrid strokeDasharray={4} />
-          <Tooltip />
+          <XAxis tick={{ fontSize: 12 }} dataKey={"label"} />
+          <YAxis
+            tick={{ fontSize: 12 }}
+            unit={"%"}
+            axisLine={false}
+            tickLine={false}
+            domain={[0, 100]}
+          />
+
+          <CartesianGrid opacity={0.2} vertical={false} />
+
+          <Tooltip content={<CustomTooltip />} />
+
           {combinationFilter &&
             combinationFilter.map((combi) => (
+              <>
+                <Area
+                  key={combi}
+                  name={`${combi} Star`}
+                  dataKey={`avg_${combi}`}
+                  type={"monotone"}
+                  strokeWidth={2}
+                  // fill={"none"}
+                  fill="url(#color)"
+                  stroke={colorArray[combi - 1]}
+                  unit={"%"}
+                />
+              </>
+            ))}
+
+          {individualStarFilter && (
+            <>
+              <defs>
+                <linearGradient id="color" x1="0" y1="0" x2="0" y2="1">
+                  <stop
+                    offset="0%"
+                    stopColor={colorArray[individualStarFilter]}
+                    stopOpacity={0.4}
+                  />
+                  <stop
+                    offset="75%"
+                    stopColor={colorArray[individualStarFilter]}
+                    stopOpacity={0.05}
+                  />
+                </linearGradient>
+              </defs>
               <Area
-                key={combi}
-                name={`${combi} Star`}
-                dataKey={`avg_${combi}`}
+                name={`${individualStarFilter} Star`}
+                dataKey={`avg_${individualStarFilter}`}
                 type={"monotone"}
                 strokeWidth={2}
-                fill={colorArray[combi - 1]}
-                stroke={colorArray[combi - 1]}
+                // fill={"none"}
+                fill="url(#color)"
+                stroke={colorArray[individualStarFilter]}
                 unit={"%"}
               />
-            ))}
-          {individualStarFilter && (
-            <Area
-              name={`${individualStarFilter} Star`}
-              dataKey={`avg_${individualStarFilter}`}
-              type={"monotone"}
-              strokeWidth={2}
-              fill={colorArray[individualStarFilter]}
-              stroke={colorArray[individualStarFilter]}
-              unit={"%"}
-            />
+            </>
           )}
+
           <Legend iconType="circle" />
         </AreaChart>
       </ResponsiveContainer>
@@ -96,4 +128,40 @@ function LineGraphFilterBar({ dispatch }) {
   );
 }
 
+function CustomTooltip({ action, payload, label }) {
+  console.log(action, payload, label);
+  return (
+    <Card padding="xs" radius="sm" withBorder>
+      <Label label={label} />
+      <StarAverage payload={payload} />
+    </Card>
+  );
+}
+function Label({ label }) {
+  return (
+    <Box align="flex-start" justify="flex-start" gap="xs">
+      <Text size="sm">% {label}</Text>
+    </Box>
+  );
+}
+function StarAverage({ payload }) {
+  return (
+    <>
+      {payload.length ? (
+        <Box align="flex-start" justify="flex-start" gap="xs">
+          {payload.map((data) => (
+            <Text
+              key={data.value}
+              size="xs"
+              style={{ color: "var(--mantine-color-dark-2)" }}
+            >
+              {data.name}: {data.value}
+              {data.unit}
+            </Text>
+          ))}
+        </Box>
+      ) : null}
+    </>
+  );
+}
 export default LineGraph;
