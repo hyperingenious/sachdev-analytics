7891c8bc6270924e1b9c14b455c703a86ab973f0
savmg <skbmasale941@gmail.com>
2023-10-08 02:54:01 +0530

    Made SquareGraph functional

---
+++ src/components/dashboard/SquareGraph.jsx
@@ -0,0 +1,52 @@
+import { Group, Text } from "@mantine/core";
+import styles from "../../assets/styles/SquareGraph.module.css";
+import { useSelector } from "react-redux";
+
+const squareColor = [
+  "#101010",
+  "#072516",
+  "#0e4429",
+  "#006d32",
+  "#26a641",
+  "#39d353",
+];
+
+function SquareGraph() {
+  const { three100Squares } = useSelector(
+    (store) => store.three100SquareFilter
+  );
+  console.log(three100Squares);
+  return (
+    <Group justify="space-between">
+      <Group justify="space-between" w={"100%"}>
+        <Text size="xs">Last 300 reviews</Text>
+        <Group gap={2}>
+          <Text size="xs">1 to 5</Text>
+          <Group gap={0}>
+            {squareColor.map((sq) => (
+              <div
+                key={sq}
+                className={styles.square}
+                style={{ background: sq }}
+              >
+                {" "}
+              </div>
+            ))}
+          </Group>
+        </Group>
+      </Group>
+
+      <div className={styles.container1}>
+        {three100Squares.map((rating, index) => (
+          <div
+            className={styles.square}
+            style={{ background: squareColor[rating] }}
+            key={index}
+          ></div>
+        ))}
+      </div>
+    </Group>
+  );
+}
+
+export default SquareGraph;
