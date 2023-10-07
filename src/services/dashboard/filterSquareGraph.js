44c33dcef47d04d355ce41623860d0196866e7df
savmg <skbmasale941@gmail.com>
2023-10-08 02:26:23 +0530

    300 days sq filter is added

---
+++ src/services/dashboard/filterSquareGraph.js
@@ -0,0 +1,27 @@
+import { rawFromServer } from "../../config/app-data";
+
+function create300Square() {
+  const len = rawFromServer.length;
+  let sq = [];
+
+  if (len >= 304) {
+    rawFromServer.slice(-304).forEach((el) => sq.push(el.rating));
+    return sq;
+  }
+
+  if (len === 0) {
+    sq = Array.from({ length: 304 }, () => 0);
+    return sq;
+  }
+
+  if (len < 304) {
+    const difference = 304 - len;
+
+    Array.from({ length: difference }, () => 0).forEach((el) => sq.push(el));
+
+    rawFromServer.forEach((el) => sq.push(el));
+    return sq;
+  }
+}
+
+export const last300Squares = create300Square();
